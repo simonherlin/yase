94d2f6c240ed63aae78e5ee0601ae192ad67381a
Herlin Simon <herlin.simon@gmail.com>
2026-09-14 06:41:00 +0200

    chore: remove CI and legacy prototype code

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.9-slim
-
-WORKDIR /app
-COPY . /app
-
-RUN apt-get update && apt-get install -y make
-RUN make install
-
-CMD ["make", "format", "lint", "tests", "sonar"]
--- src/yase/features.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def create_features(df: pd.DataFrame) -> pd.DataFrame:
-    """Create new features from existing data."""
-    # Placeholder for feature engineering code
-    return df
